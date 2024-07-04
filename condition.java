e833aae4b6353bc2cfe2525bfa45ed5fe47dfa9b
Rajendra <rajendrarauta979@gmail.com>
2024-07-04 19:36:38 +0530

    ..

--- condition.java
+++ condition.java
@@ -1,13 +1,32 @@
 import java.util.*;
-public class condition{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        if (age > 18) {
-          System.out.println("adult");
-        } else {
-          System.out.println("you are not adult ");
-        }
+// public class condition{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int age = sc.nextInt();
+//         if (age > 18) {
+//           System.out.println("adult");
+//         } else {
+//           System.out.println("you are not adult ");
+//         }
     
-      }
-    }
+//       }
+//     }
+
+
+
+public class condition{
+      public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+        int a =  sc.nextInt();
+        int b =  sc.nextInt();
+
+            if (a==b) {
+                System.out.println("The tow number are equale");
+            }
+            else if(a>b){
+                System.out.println("a is greater then b");
+            }else {
+                System.out.println("a is lesser");
+            }
+}}
