cf4bc9a311b6927f42f96c3f2daf4895e18f39ab
Rajendra <rajendrarauta979@gmail.com>
2024-07-04 23:18:36 +0530

    patten

--- looping.java
+++ looping.java
@@ -1,11 +1,38 @@
+
+// // sum of n natural number 
+// import java.util.*;
+
+// public class looping {
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         int num = sc.nextInt();
+//         int sum =0;
+//         for (int i = 1; i < num + 1; i++) {
+//             sum = sum + i;
+
+//         }
+//         System.out.println(sum);
+
+//     }
+// }
+
+// multiple table
+import java.util.*;
+
 public class looping {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+       int num =sc.nextInt();
+       System.out.println("our table is ");
+      for(int i =1; i<11 ;i++){
 
-        for(int i=0; i<100;i++){
-            System.out.println(i);
-        }
+    //    int  mul = num*i;
+  
+        System.out.println(num*i);
+      }
+
     }
-
-
 }
