94665233448fafb255aa7f5e89f0be477656deea
Rajendra <rajendrarauta979@gmail.com>
2024-07-05 19:45:40 +0530

    patten

--- patten.java
+++ patten.java
@@ -225,51 +225,185 @@
 // **      **
 // *        *
 
+// public class patten {
+
+//     public static void main(String[] args) {
+
+//         int n = 5;
+//         // upper half
+//         for (int i = 1; i <= n; i++) {
+//             // 1st part
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+
+//             // space
+//             int spaces = 2 * (n - i);
+//             for (int j = 1; j <= spaces; j++) {
+//                 System.out.print(" ");
+//             }
+//             // 2nd half
+
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+//         // lower half
+
+//         for (int i = n; i >= 1; i--) {
+//             // 1st part
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+
+//             // space
+//             int spaces = 2 * (n - i);
+//             for (int j = 1; j <= spaces; j++) {
+//                 System.out.print(" ");
+//             }
+//             // 2nd half
+
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+// public class patten {
+
+//         public static void main(String[] args) {
+
+//             int n = 5;
+//             for(int i=1;i<=n;i++){
+//                 for(int j=1;j<=n-i;j++){
+//                     System.out.print(" ");
+//             }
+//             // stars  
+//             for(int j=1;j<=n;j++){
+//                 System.out.print("*");
+//             }
+//                 System.out.println();
+
+//         }
+//         }}
+
+// number pyramid 
+//     1 
+//    2 2
+//   3 3 3
+//  4 4 4 4
+// 5 5 5 5 5
+
+// public class patten {
+
+//     public static void main(String[] args) {
+
+//         int n = 5;
+
+//         for (int i = 1; i <= n; i++) {
+//             // spaces
+//             for (int j = 1; j <= n - i; j++) {
+//                 System.out.print(" ");
+//             }
+//             // numbers -> print row number . row no time3
+//             for(int j=1;j<=i ;j++){
+//                 System.out.print(i+" ");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+
+// }
+
+// // palindrom patten 
+//     1
+//    212
+//   32123
+//  4321234
+// 543212345
+
+// public class patten {
+
+//         public static void main(String[] args) {
+
+//             int n = 5;
+
+//             // outer loop 
+//             for(int i=1; i<=n ;i++){
+//                 // space 
+//                 for(int j=1;j<=n-i;j++){
+//                     System.out.print(" ");
+//                 }
+//                 // 1st half numbers 
+//                 for(int j=i;j>=1;j--){
+//                     System.out.print(j);
+//                 }
+//                 // 2nd half number 
+//                 for(int j=2;j<=i;j++){
+//                     System.out.print(j);
+//                 }
+//                 System.out.println();
+//             }
+
+//         }
+
+//     }
+
+
+
+
+//     *
+//    ***
+//   *****
+//  *******
+//  *******
+//   *****
+//    ***
+//     *
 public class patten {
 
     public static void main(String[] args) {
 
-        int n = 5;
-        // upper half
-        for (int i = 1; i <= n; i++) {
-            // 1st part
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
+        int n = 4;
 
+        // outer loop
+        for (int i = 1; i <= n; i++) {
             // space
-            int spaces = 2 * (n - i);
-            for (int j = 1; j <= spaces; j++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            // 2nd half
-
-            for (int j = 1; j <= i; j++) {
+            // stars 
+            for(int j=1;j<=2*i-1;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        // lower half
-
+        // lower half 
         for (int i = n; i >= 1; i--) {
-            // 1st part
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-
             // space
-            int spaces = 2 * (n - i);
-            for (int j = 1; j <= spaces; j++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            // 2nd half
-
-            for (int j = 1; j <= i; j++) {
+            // stars 
+            for(int j=1;j<=2*i-1;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
 
     }
+
 }
