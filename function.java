43d0471d22f831fb2fd7e0ed48e4592e09abb574
Rajendra <rajendrarauta979@gmail.com>
2024-07-05 20:19:08 +0530

    function

---
+++ function.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+// public class function {
+//     public static void printMyname(String name){
+//         System.out.println(name);
+//         return;
+//     }
+//     public static void main(String args[]){
+
+//         Scanner sc =new Scanner(System.in);
+//         String name =sc.next();
+//         printMyname(name);
+//     }
+// }
+
+
+
+// public class function {
+//     public static int  calculateSum(int a ,int b){
+//         int sum = a+b;
+//         return sum;
+//     }
+//     public static void main(String args[]){
+
+//         Scanner sc =new Scanner(System.in);
+//         int a =sc.nextInt();
+//        int b =sc.nextInt();
+       
+//        int sum = calculateSum(a, b);
+//        System.out.println("sum of two number :"+sum);
+//     }
+// }
+
+
+
+
+
+// public class function {
+//     public static int  calculateMul(int a ,int b){
+//         int mul = a*b;
+//         return mul;
+//     }
+//     public static void main(String args[]){
+
+//         Scanner sc =new Scanner(System.in);
+//         int a =sc.nextInt();
+//        int b =sc.nextInt();
+       
+//        int mul = calculateMul(a, b);
+//        System.out.println("product of two number :"+mul);
+//     }
+// }
+
+
+
+
+
+// factorial of number 
+
+
+
+public class function {
+    public static int  calcuFact(int num){
+        for
+        return mul;
+    }
+    public static void main(String args[]){
+
+        Scanner sc =new Scanner(System.in);
+        int a =sc.nextInt();
+       int b =sc.nextInt();
+       
+       int mul = calculateMul(a, b);
+       System.out.println("product of two number :"+mul);
+    }
+}
