864cdeb15b63bfe76eb055fa6c70bb37cdee8c11
Rajendra <rajendrarauta979@gmail.com>
2024-07-05 09:04:42 +0530

    patten

--- patten.java
+++ patten.java
@@ -85,9 +85,6 @@
 //         }
 //     }
 
-
-
-
 // *
 // **
 // ***
@@ -111,13 +108,10 @@
 //         }
 
 //         System.out.println();
-       
+
 //     }
 // }
 // }
-
-
-
 
 // 1 
 // 1 2
@@ -125,19 +119,157 @@
 // 1 2 3 4
 // 1 2 3 4 5
 
+// public class patten {
+
+//     public static void main(String[] args) {
+
+//         int n = 5;
+
+//        // outer loop 
+//        for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             System.out.print(j + " ");
+//         }
+//         System.out.println();
+//        }
+
+// }
+// }
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+// public class patten {
+
+//     public static void main(String[] args) {
+
+//         int n = 5;
+
+//        // outer loop 
+//        for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i+1;j++){
+//             System.out.print(j);
+//         }
+//         System.out.println();
+//        }
+
+// }
+// }
+
+// Floyd trangle 
+
+// 1 
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+
+// public class patten {
+
+//     public static void main(String[] args) {
+
+//         int n = 5;
+//         int number = 1;
+
+//        // outer loop 
+//        for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             System.out.print(number+ " ");
+//             number++;
+//         }
+//         System.out.println();
+//        }
+
+// }
+// }
+
+// 1 
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+
+// public class patten {
+
+//     public static void main(String[] args) {
+
+//         int n = 5;
+
+//         // outer loop
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 int sum = i + j;
+//                 if (sum % 2 == 0) {
+//                     System.out.print("1 ");
+//                 } else {
+//                     System.out.print("0 ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+
 public class patten {
 
     public static void main(String[] args) {
 
         int n = 5;
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            // 1st part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
 
-       // outer loop 
-       for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            System.out.print(j+ " ");
+            // space
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            // 2nd half
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        System.out.println();
-       }
-    
-}
+
+        // lower half
+
+        for (int i = n; i >= 1; i--) {
+            // 1st part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // space
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            // 2nd half
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 }
