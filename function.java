5c7907a219bd09abb7a4ad6df01ad245f5214ca9
Rajendra <rajendrarauta979@gmail.com>
2024-07-05 20:25:40 +0530

    function

--- function.java
+++ function.java
@@ -13,8 +13,6 @@ import java.util.*;
 //     }
 // }
 
-
-
 // public class function {
 //     public static int  calculateSum(int a ,int b){
 //         int sum = a+b;
@@ -25,15 +23,11 @@ import java.util.*;
 //         Scanner sc =new Scanner(System.in);
 //         int a =sc.nextInt();
 //        int b =sc.nextInt();
-       
+
 //        int sum = calculateSum(a, b);
 //        System.out.println("sum of two number :"+sum);
 //     }
 // }
-
-
-
-
 
 // public class function {
 //     public static int  calculateMul(int a ,int b){
@@ -45,32 +39,35 @@ import java.util.*;
 //         Scanner sc =new Scanner(System.in);
 //         int a =sc.nextInt();
 //        int b =sc.nextInt();
-       
+
 //        int mul = calculateMul(a, b);
 //        System.out.println("product of two number :"+mul);
 //     }
 // }
 
-
-
-
-
 // factorial of number 
 
-
-
 public class function {
-    public static int  calcuFact(int num){
-        for
-        return mul;
-    }
-    public static void main(String args[]){
+    public static void printFact(int num) {
+        if(num<0){
+            System.out.println("Invalid Number");
+            return;
 
-        Scanner sc =new Scanner(System.in);
-        int a =sc.nextInt();
-       int b =sc.nextInt();
-       
-       int mul = calculateMul(a, b);
-       System.out.println("product of two number :"+mul);
+        }
+        int factorial = 1;
+        for (int i = num; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+
+        System.out.println(factorial);
+
+    }
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        printFact(num);
+
     }
 }
