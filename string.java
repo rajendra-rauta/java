052d915fa05ed70a0991b62eec0c821ea9f23c8f
Rajendra <rajendrarauta979@gmail.com>
2024-07-06 10:53:26 +0530

    string

---
+++ string.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+// public class string {
+//     public static void main (String args[]){
+//     //  concatination 
+//     String firstName = "Rajendra";
+//     String lastName = "Rauta";
+//     String fullName = firstName + " "+lastName;
+//     System.out.println(fullName.length());
+
+
+//     // charAt 
+
+//     for(int i=0;i<fullName.length();i++){
+//         System.out.println(fullName.charAt(i));
+//     }
+
+
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+public class string {
+    public static void main (String args[]){
+    //  concatination 
+    String name1 = "Rajendra";
+    String name2 = "Rauta";
+  
+
+        if(name1.compareTo(name2)==0){
+            System.out.println("String are equle");
+        }
+        else{
+            System.out.println("Strings are not equal");
+        }
+    }
+
+
+    }
+
+
+
