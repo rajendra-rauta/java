426cca971d5b4e6f763389e905a7f66eb5125258
Rajendra <rajendrarauta979@gmail.com>
2024-07-07 21:05:34 +0530

    string

---
+++ string1.java
@@ -0,0 +1,20 @@
+public class string1 {
+    
+
+        public static void main (String args[]){
+        //  concatination 
+        String name1 = "Rajendra";
+        String name2 = "Rauta";
+      
+    
+            if(name1.compareTo(name2)==0){
+                System.out.println("String are equle");
+            }
+            else{
+                System.out.println("Strings are not equal");
+            }
+        } 
+    
+        }
+      
+
