88d58ab198530d2767dc916a498f1d60fc986279
Rajendra <rajendrarauta979@gmail.com>
2024-07-09 07:08:52 +0530

    bit

--- bitManupalition.java
+++ bitManupalition.java
@@ -29,24 +29,49 @@ import java.util.*;;
 
 // }
 
+// public class bitManupalition {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int oper = sc.nextInt();
+//         int n = 5;
+//         int pos = 1;
+
+//         int bitMask = 1<<pos;
+//         if (oper == 1) {
+//             // set
+//             int newNumber = bitMask | n;
+//             System.out.println(newNumber);
+//         } else {
+//             int newBitmark = ~(bitMask);
+//             int newNumber = newBitmark & n;
+//             System.out.println(newNumber);
+//         }
+
+//     }
+
+// }
+
+
+
+
 public class bitManupalition {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int oper = sc.nextInt();
-        int n = 5;
-        int pos = 1;
-
-        int bitMask = 1<<pos;
-        if (oper == 1) {
-            // set
-            int newNumber = bitMask | n;
-            System.out.println(newNumber);
-        } else {
-            int newBitmark = ~(bitMask);
-            int newNumber = newBitmark & n;
-            System.out.println(newNumber);
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            int oper = sc.nextInt();
+            int n = 5;
+            int pos = 1;
+    
+            int bitMask = 1<<pos;
+            if (oper == 1) {
+                // set
+                int newNumber = bitMask | n;
+                System.out.println(newNumber);
+            } else {
+                int newBitmark = ~(bitMask);
+                int newNumber = newBitmark & n;
+                System.out.println(newNumber);
+            }
+    
         }
-
-    }
-
-}
+    
+    // }
