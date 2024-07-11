df7b5cdc59eeb95c30d529fcea6ca5d553877a36
Rajendra <rajendrarauta979@gmail.com>
2024-07-11 08:35:13 +0530

    bit mapulation

--- bitManupalition.java
+++ bitManupalition.java
@@ -54,25 +54,3 @@ import java.util.*;;
 
 
 
-public class bitManupalition {
-        public static void main(String args[]) {
-            Scanner sc = new Scanner(System.in);
-            int oper = sc.nextInt();
-            int n = 5;
-            int pos = 1;
-    
-            int bitMask = 1<<pos;
-            if (oper == 1) {
-                // set
-                int newNumber = bitMask | n; 
-                System.out.println(newNumber);
-            } else {
-                int newBitmark = ~(bitMask);
-                int newNumber = newBitmark & n;
-                System.out.println(newNumber);
-            }
-            
-    
-        }
-    
-    // }
