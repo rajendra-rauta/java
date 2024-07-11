eceb1c64a9a5a6d4f9713b99b9cde759950921c5
Rajendra <rajendrarauta979@gmail.com>
2024-07-11 17:33:43 +0530

    inserction sort

--- Dsa/sorting.java
+++ Dsa/sorting.java
@@ -2,6 +2,81 @@ package Dsa;
 
 import java.util.*;
 
+// public class sorting {
+//     public static void printArray(int arr[]) {
+//         for (int i = 0; i < arr.length; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+
+//     public static void main(String args[]) {
+//         int arr[] = { 7, 8, 3, 1, 2 };
+
+//         // bubble sort
+//         for (int i = 0; i < arr.length - 1; i++) {
+//             for (int j = 0; j < arr.length - i - 1; j++) {
+//                 if (arr[j] > arr[j + 1]) {
+//                     // swap
+//                     int temp = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = temp;
+
+//                 }
+
+//             }
+//         }
+//         printArray(arr);
+
+//     }
+
+// }
+
+
+
+
+//<----------- secelection sort  -------->
+
+// public class sorting {
+//     public static void printArray(int arr[]) {
+//         for (int i = 0; i < arr.length; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+
+//     public static void main(String args[]) {
+//         int arr[] = { 7, 8, 3, 1, 2 };
+
+//         // selection sort
+//         for (int i = 0; i < arr.length-1; i++) {
+//             int smallest = i;
+//             for (int j = i + 1; j < arr.length; j++) {
+//                 if(arr[smallest]> arr[j]){
+//                     smallest =j;
+//                 }
+             
+//             }
+//             int temp = arr[smallest];
+//             arr[smallest]= arr[i];
+//             arr[i]=temp;
+
+//         }
+
+//         printArray(arr);
+//     }
+
+// }
+
+
+
+
+
+
+// <------------binserction sort  ----------->
+
+
+
+
+
 public class sorting {
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
@@ -12,21 +87,18 @@ public class sorting {
     public static void main(String args[]) {
         int arr[] = { 7, 8, 3, 1, 2 };
 
-        // bubble sort
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-
-                }
-
-            }
+     //inserction sort
+     for(int i=1;i<arr.length;i++){
+        int current = arr[i];
+        int j= i-1;
+        while(j>=0 && current < arr[j]){
+            arr[j+1]= arr[j];
+            j--;
         }
+        // placement
+        arr[j+1]= current;
+     }
         printArray(arr);
-
     }
 
 }
