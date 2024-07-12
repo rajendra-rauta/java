0e854fb0fdd650318dfcfa839f22807481ce5360
Rajendra <rajendrarauta979@gmail.com>
2024-07-12 09:15:28 +0530

    recursion

--- Dsa/recursion1.java
+++ Dsa/recursion1.java
@@ -1,15 +1,34 @@
 package Dsa;
 
+// public class recursion1 {
+//     public static void printNum(int n){
+//         if(n== 0){
+//             return;
+//         }
+//         System.out.println(n);
+//         printNum(n-1);
+//     }
+//     public static void main(String args[]){
+//         int n=5;
+//         printNum(n);
+//     }
+// }
+
+
+
+
+
+
 public class recursion1 {
     public static void printNum(int n){
-        if(n== 0){
+        if(n== 6){
             return;
         }
         System.out.println(n);
-        printNum(n-1);
+        printNum(n+1);
     }
     public static void main(String args[]){
-        int n=5;
+        int n=1;
         printNum(n);
     }
 }
