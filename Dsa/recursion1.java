f694abcdbaf34582006ee2a7ab295d20f4557c85
Rajendra <rajendrarauta979@gmail.com>
2024-07-12 16:12:52 +0530

    recursion

--- Dsa/recursion1.java
+++ Dsa/recursion1.java
@@ -81,23 +81,90 @@ package Dsa;
 // fibonaic series 
 
 
+// public class recursion1 {
+//     public static void printFib(int a,int b, int n) {
+//         if(n==0){
+//             return;
+//         }
+//         int c=a+b;
+//         System.out.println(c);
+//         printFib(b, c, n-1);
+//     }
+
+//     public static void main(String args[]) {
+//         int a=0, b=1;
+//         System.out.println(a);
+//         System.out.println(b);
+//         int n=7;
+
+//         printFib(a, b, n-2);
+
+//     }
+// }
+
+
+
+
+// print x^n
+
+
+
+
+// public class recursion1 {
+//     public static int calPow(int x, int n){
+//         if(n==0){  // base case 1
+//             return 1;
+//         }
+//         if(x==0){ // base case 2 
+//             return 0;
+//         }
+//         int xpownm1 = calPow(x, n-1);
+//         int xpown = x * xpownm1;
+//         return xpown;
+//     }
+
+
+//     public static void main(String args[]) {
+//         int x=2, n=5;
+//         int ans = calPow(x, n);
+//         System.out.println(ans);
+
+//     }
+// }
+
+
+
+
+// print x^n (stack height = logn)
+
+
+
 public class recursion1 {
-    public static void printFib(int a,int b, int n) {
-        if(n==0){
-            return;
+    public static int calPow(int x, int n){
+        if(n==0){  // base case 1
+            return 1;
         }
-        int c=a+b;
-        System.out.println(c);
-        printFib(b, c, n-1);
+        if(x==0){ // base case 2 
+            return 0;
+        }
+      // if n is even
+      if(n %2 == 0){
+        return calPow(x, n/2) * calPow(x, n/2);
+      }
+      else{ // n is odd
+       return calPow(x, n/2) * calPow(x, n/2) * x;
+
+      }
     }
 
-    public static void main(String args[]) {
-        int a=0, b=1;
-        System.out.println(a);
-        System.out.println(b);
-        int n=7;
 
-        printFib(a, b, n-2);
+    public static void main(String args[]) {
+        int x=2, n=5;
+        int ans = calPow(x, n);
+        System.out.println(ans);
 
     }
 }
+
+
+
