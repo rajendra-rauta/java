0a83fd4deb5fbb829fee7364d288bc961018638d
Rajendra <rajendrarauta979@gmail.com>
2024-07-13 10:17:07 +0530

    recursion 2

--- Dsa/recursion_2.java
+++ Dsa/recursion_2.java
@@ -90,30 +90,135 @@ package Dsa;
 
 // move all 'x' to the end of the string 
 
+// public class recursion_2 {
+//     public static void moveAllx(String str, int idx, int count, String newString) {
+//         if (idx == str.length()) {
+//             for (int i = 0; i < count; i++) {
+//                 newString += 'x';
+//             }
+//             System.out.println(newString);
+//             return;
+//         }
+
+//         char currChar = str.charAt(idx);
+//         if (currChar == 'x') {
+//             count++;
+//             moveAllx(str, idx + 1, count, newString);
+//         } else {
+//             newString += currChar;
+//             moveAllx(str, idx + 1, count, newString);
+//         }
+
+//     }
+
+//     public static void main(String args[]) {
+//         String str = "axbcxxd";
+//         moveAllx(str, 0, 0, " ");
+
+//     }
+// }
+
+// remove duplicate in a string 
+
+// public class recursion_2 {
+//     public static boolean[] map = new boolean[26];
+
+//     public static void removeDuplicates(String str, int idx, String newstr) {
+
+//         if (idx == str.length()) {
+//             System.out.println(newstr);
+//             return;
+//         }
+//         char currChar = str.charAt(idx);
+//         if (map[currChar - 'a']) {
+//             removeDuplicates(str, idx + 1, newstr);
+
+//         } else {
+//             newstr += currChar;
+//             map[currChar - 'a'] = true;
+//             removeDuplicates(str, idx + 1, newstr);
+//         }
+
+//     }
+
+//     public static void main(String args[]) {
+//         String str = "abbccda";
+//         removeDuplicates(str, 0, "");
+
+//     }
+// }
+
+// print all the subsequences of a string 
+
+// public class recursion_2 {
+//     public static void subSequence(String str , int idx, String newString){
+//         if(idx == str.length()){
+//             System.out.println(newString);
+//             return ;
+//         }
+//         char currchar = str.charAt(idx);
+//         // to be 
+//         subSequence(str, idx+1, newString+currchar);
+//         // or not to be 
+//         subSequence(str, idx+1, newString);
+//     }
+//     public static void main(String args[]) {
+// String str = "abc";
+// subSequence(str, 0, "");
+//     }
+// }
+
+// print all the unique subSequence of a string 
+
+// import java.util.HashSet;
+
+// public class recursion_2 {
+//     public static void printSubseq(String str, int idx, String res, HashSet<String> allSubseq) {
+//         if (idx == str.length()) {
+//             if (allSubseq.contains(res)) {
+//                 return;
+//             }
+//             allSubseq.add(res);
+//             System.out.println(res);
+//             return;
+//         }
+
+//         // choose
+//         printSubseq(str, idx + 1, res + str.charAt(idx), allSubseq);
+//         // don't choose
+//         printSubseq(str, idx + 1, res, allSubseq);
+//     }
+
+//     public static void main(String args[]) {
+//         String str1 = "abc";
+//         String str2 = "aaa";
+//         HashSet<String> allSubseq = new HashSet<>();
+//         printSubseq(str2, 0, "", allSubseq);
+//     }
+// }
+
+
+// print keyboard combination 
+import java.util.HashSet;
+
 public class recursion_2 {
-    public static void moveAllx(String str, int idx, int count, String newString) {
-        if (idx == str.length()) {
-            for (int i = 0; i < count; i++) {
-                newString += 'x';
-            }
-            System.out.println(newString);
+    public static String keypad[] = { ".", "abc", "def", "ghi", "jkl", "mno", "pqrs",
+            "tu", "vwx", "yz" };
+
+    public static void printKeypadCombination(String number, int idx, String res) {
+        if (idx == number.length()) {
+            System.out.println(res);
             return;
         }
 
-        char currChar = str.charAt(idx);
-        if (currChar == 'x') {
-            count++;
-            moveAllx(str, idx + 1, count, newString);
-        } else {
-            newString += currChar;
-            moveAllx(str, idx + 1, count, newString);
+        for (int i = 0; i < keypad[number.charAt(idx) - '0'].length(); i++) {
+            char curr = keypad[number.charAt(idx) - '0'].charAt(i);
+            printKeypadCombination(number, idx + 1, res + curr);
         }
-
     }
 
-    public static void main(String args[]) {
-        String str = "axbcxxd";
-        moveAllx(str, 0, 0, " ");
-
-    }
+public static void main(String args[]) {
+String number = "4";
+printKeypadCombination(number, 0, "");
+}
 }
