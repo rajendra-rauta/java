2ddb4eafab103331bbd4d6259cb8082d1a0d9b97
Rajendra <rajendrarauta979@gmail.com>
2024-07-15 21:50:01 +0530

    linked list

--- Dsa/Node.java
+++ Dsa/Node.java
@@ -1,4 +1,4 @@
-// package Dsa;
+//  package Dsa;
 
 // class LL {
 //     Node head;
@@ -119,10 +119,35 @@
 // }
 
 
+
+
+
+ package Dsa;
+
 import java.util.*;;
 class LL {
     public static void main(String args[]){
-        LinkedList<
+        LinkedList<String> list = new LinkedList<String>();
 
+        list.addFirst("a");
+        list.addFirst("is");
+        System.out.println(list);
+
+        list.addFirst("this");
+     
+        list.addLast("list");
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+        for(int i=0; i<list.size(); i++){
+            System.out.print(list.get(i) + " -> ");
+
+        }
+        System.out.println("null");
+
+
+       list.remove(3);
+       System.out.println(list);
     }
 }
