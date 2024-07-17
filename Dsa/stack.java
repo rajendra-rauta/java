13be28e7c140fb4734999da42f7094452e522465
Rajendra <rajendrarauta979@gmail.com>
2024-07-17 22:56:09 +0530

    srtack

--- Dsa/stack.java
+++ Dsa/stack.java
@@ -64,11 +64,6 @@
 
 // }
 
-
-
-
-
-
 // by the help of arrayList 
 
 // package Dsa;
@@ -105,7 +100,7 @@
 //                 return -1; 
 //             }           
 //            return list.get(list.size()-1);
-            
+
 //           }
 //     }
 
@@ -125,29 +120,103 @@
 
 // }
 
-
-
-
-
 // by the help of freamwork 
 
+// package Dsa;
+
+// import java.util.*;
+
+// public class stack {
+
+//     public static void main(String args[]) {
+//       Stack<Integer> s= new Stack<>();
+//         s.push(1);
+//         s.push(2);
+//         s.push(3);
+//         s.push(4);
+
+//         while (!s.isEmpty()) {
+//             System.out.println(s.peek());
+//             s.pop();
+//         }
+//     }
+
+// }
+
+// <----push at the bottom of stack --->
+
+
+// package Dsa;
+
+// import java.util.*;
+
+// public class stack {
+//     public static void pushAtBottom(int data, Stack<Integer> s) {
+//         if (s.empty()) {
+//             s.push(data);
+//             return;
+            
+//         }
+//         int top = s.pop();
+//         pushAtBottom(data, s);
+//         s.push(top);
+
+
+//     }
+
+//     public static void main(String args[]) {
+//         Stack<Integer> s = new Stack<>();
+//         s.push(1);
+//         s.push(2);
+//         s.push(3);
+
+//         pushAtBottom(4, s);
+
+//         while (!s.isEmpty()) {
+//             System.out.println(s.peek());
+//             s.pop();
+//         }
+//     }
+
+// }
 
 
 
 
-
+// To reverse a stack
 package Dsa;
 
 import java.util.*;
 
 public class stack {
+    public static void pushAtBottom(int data, Stack<Integer> s) {
+        if (s.empty()) {
+            s.push(data);
+            return;
+            
+        }
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+    }
 
+    public static void reverse(Stack<Integer> s){
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(top, s);
+    }
     public static void main(String args[]) {
-      Stack<Integer> s= new Stack<>();
+        Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-        s.push(4);
+
+
+        reverse(s);
+       
 
         while (!s.isEmpty()) {
             System.out.println(s.peek());
