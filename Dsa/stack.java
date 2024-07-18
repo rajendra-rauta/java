cdacd1c216e780d9b07c13ab167fcf02ac422ce6
Rajendra <rajendrarauta979@gmail.com>
2024-07-18 19:43:44 +0530

    stack

--- Dsa/stack.java
+++ Dsa/stack.java
@@ -183,6 +183,51 @@
 
 
 
+// // To reverse a stack
+// package Dsa;
+
+// import java.util.*;
+
+// public class stack {
+//     public static void pushAtBottom(int data, Stack<Integer> s) {
+//         if (s.empty()) {
+//             s.push(data);
+//             return;
+            
+//         }
+//         int top = s.pop();
+//         pushAtBottom(data, s);
+//         s.push(top);
+//     }
+
+//     public static void reverse(Stack<Integer> s){
+//         if (s.isEmpty()) {
+//             return;
+//         }
+//         int top = s.pop();
+//         reverse(s);
+//         pushAtBottom(top, s);
+//     }
+//     public static void main(String args[]) {
+//         Stack<Integer> s = new Stack<>();
+//         s.push(1);
+//         s.push(2);
+//         s.push(3);
+
+
+//         reverse(s);
+       
+
+//         while (!s.isEmpty()) {
+//             System.out.println(s.peek());
+//             s.pop();
+//         }
+//     }
+
+// }
+  
+
+
 // To reverse a stack
 package Dsa;
 
