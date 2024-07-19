8deb087580602f961a58e48570d038d5e4a7f71f
Rajendra <rajendrarauta979@gmail.com>
2024-07-19 20:11:07 +0530

    array

--- Dsa/Queuey.java
+++ Dsa/Queuey.java
@@ -5,6 +5,7 @@ public class Queuey {
         static int arr[];
         static int size ;
         static int rear = -1;
+        static int front = -1;
 
         Queue(int n ){
             arr = new int[n];
@@ -12,18 +13,26 @@ public class Queuey {
         }
 
         public static boolean isEmpty() {
-            return rear == -1;
+            return rear == -1 && front == -1;
         }
         
-        // enqueue
 
+        public static boolean isFull() {
+            return (rear+1) % size == front;
+        }
+        // enqueue
         public static void add(int data){
-            if (rear == size-1) {
+            if (isFull()) {
                 System.out.println("full queue");
                 return ;
                 
             }
-            rear ++;
+            // first element add
+            if (front == -1) {
+                front= 0;
+                
+            }
+            rear = (rear +1) %size;
             arr[rear] = data;
         }
 
@@ -33,13 +42,14 @@ public class Queuey {
                 System.out.println("empty queue");
                 return -1;
             }
-            int front = arr[0];
-            for(int i= 0; i<rear; i++){
-                arr[i] = arr[i+1];
-
-            }
-            rear--;
-            return front;
+            int result = arr[front];
+           if (rear == front) {
+            rear = front = -1;
+           }
+           else{
+            front = (front +1) % size;
+           }
+            return result;
         }
 
 
@@ -49,10 +59,8 @@ public class Queuey {
                 System.out.println("empty queue");
                 return -1;
             }
-         return arr[0];
+         return arr[front];
         }
-
-    
         
     }
     
@@ -61,6 +69,13 @@ public class Queuey {
         q.add(1);
         q.add(2);
         q.add(3);
+        q.add(4);
+        q.add(5);
+
+        System.out.println(q.remove());
+        q.add(6);
+        System.out.println(q.remove());
+        q.add(7);
 
         while (!q.isEmpty()) {
             System.out.println(q.peek());
