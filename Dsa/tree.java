82fbad0166dc4c00538ada3439233b6d32a4d80c
Rajendra <rajendrarauta979@gmail.com>
2024-08-04 20:59:17 +0530

    tree

--- Dsa/tree.java
+++ Dsa/tree.java
@@ -31,7 +31,7 @@ public class tree {
        
         
     }
-
+ 
     public static void main(String args[]) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
